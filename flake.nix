16cbc264b5e7baf09cb895e8c5a49f7d915399d8
Ali Almahdi <ali@almahdi.cc>
2023-10-29 13:47:26 +0300

    Added aarch64

--- flake.nix
+++ flake.nix
@@ -31,5 +31,31 @@
       program = "${self.packages.x86_64-linux.thorium}/bin/thorium";
     };
     apps.x86_64-linux.default = self.apps.x86_64-linux.thorium;
+    # -----------------------------------------------------------
+    packages.aarch64-linux.thorium = let
+      pkgs = import nixpkgs { system = "aarch64-linux"; };
+      name = "thorium";
+      version = "117.0.5938.157 - 53";
+      src = pkgs.fetchurl {
+        url = "https://github.com/Alex313031/Thorium-Raspi/releases/download/M117.0.5938.157/Thorium_Browser_117.0.5938.157_arm64.AppImage";
+        sha256 = "";
+      };
+      appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
+    in
+    pkgs.appimageTools.wrapType2 {
+      inherit name version src;
+      extraInstallCommands = ''
+        install -m 444 -D ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
+        install -m 444 -D ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
+        substituteInPlace $out/share/applications/thorium-browser.desktop \
+    	  --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${name} %U'
+      '';
+    };
+    packages.aarch64-linux.default = self.packages.aarch64-linux.thorium;
+    apps.aarch64-linux.thorium = {
+      type = "app";
+      program = "${self.packages.aarch64-linux.thorium}/bin/thorium";
+    };
+    apps.aarch64-linux.default = self.apps.aarch64-linux.thorium;
   };
 }
